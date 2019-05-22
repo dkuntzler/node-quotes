779fd5648f6c2b3233098ef12cae06fda69efb39
Dana Lynn Kuntzler <Dana@Danas-MacBook-Pro.local>
2019-05-22 12:41:13 -0400

    first commit

---
+++ quotes.js
@@ -0,0 +1,5 @@
+const quotes = ['It’s just a flesh wound.', 'I am serious. And don’t call me Shirley.', 'I’m just one stomach flu away from my goal weight.', 'Martini. Gin, not vodka. Obviously. Stirred for 10 seconds while glancing at an unopened bottle of vermouth.', 'Greater good? I am your wife! I’m the greatest good you’re ever gonna get!'];
+
+module.exports =  {
+    quotes
+};
